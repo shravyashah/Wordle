56aff1ec44ff010e0a572f73648ca2151d86fef8
shravyashah <shravya.shah066@gmail.com>
2026-08-26 22:17:17 -0600

    keyboard styled and started working on logic of clicking letter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
     
     createSquares();
+    let guessedWords = [[]]
+    let availableSpace = 1;
     
-    function createSquares(){
+    const keys = document.querySelectorAll(".keyboard-row button")
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key")
+
+            updateGuessedWords(letter)
+        }        
+    }
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length
+        return guessedWords[numberOfGuessedWords-1]
+
+    }
+
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr()
+
+        if (currentWordArr && currentWordArr.length < 5){
+            currentWordArr.push(letter)
+
+            const availableSpaceE = document.getElementById(String(1))
+            availableSpace = availableSpace + 1
+            availableSpaceE.textContent = letter;
+        }
+    }
+    
+    function createSquares() {
         
         const gameBoard = document.getElementById("board")
         for (let index = 0; index < 30; index++) {
