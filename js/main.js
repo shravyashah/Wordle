06d385e43401ec957883d127c155f418ef947021
shravyashah <shravya.shah066@gmail.com>
2026-09-13 14:49:16 -0600

    added some more formating and allowing for word input

--- js/main.js
+++ js/main.js
@@ -1,46 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
     
     createSquares();
-    let guessedWords = [[]]
+    let guessedWords = [[]];
     let availableSpace = 1;
+
+    let word = 'super';
     
-    const keys = document.querySelectorAll(".keyboard-row button")
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWords.length;
+        return guessedWords[numberOfGuessedWords-1];
+
+    }
+
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr();
+
+        if (currentWordArr && currentWordArr.length < 5){
+            currentWordArr.push(letter);
+
+            const availableSpaceE = document.getElementById(String(availableSpace));
+            availableSpace = availableSpace + 1;
+            availableSpaceE.textContent = letter;
+        }
+    }
+    
+    function handleSubmitWord() {
+        const currentWordArr = getCurrentWordArr();
+        if (currentWordArr.length !== 5){
+            window.alert("Word must be 5 letters");
+        }
+        const currentWord = currentWordArr.join('');
+        if (currentWord === word) {
+            window.alert("Congratulations! You guessed the word!");
+        }
+        guessedWords.push([]);
+
+    }
+
+    function createSquares() { 
+        const gameBoard = document.getElementById("board");
+        for (let index = 0; index < 30; index++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", index + 1);
+            gameBoard.appendChild(square);
+            
+        }
+    }
 
     for (let i = 0; i < keys.length; i++) {
         keys[i].onclick = ({ target }) => {
             const letter = target.getAttribute("data-key")
 
+            if (letter === 'enter') {
+                handleSubmitWord()
+                return;
+            }
+
             updateGuessedWords(letter)
         }        
-    }
-
-    function getCurrentWordArr() {
-        const numberOfGuessedWords = guessedWords.length
-        return guessedWords[numberOfGuessedWords-1]
-
-    }
-
-    function updateGuessedWords(letter) {
-        const currentWordArr = getCurrentWordArr()
-
-        if (currentWordArr && currentWordArr.length < 5){
-            currentWordArr.push(letter)
-
-            const availableSpaceE = document.getElementById(String(1))
-            availableSpace = availableSpace + 1
-            availableSpaceE.textContent = letter;
-        }
-    }
-    
-    function createSquares() {
-        
-        const gameBoard = document.getElementById("board")
-        for (let index = 0; index < 30; index++) {
-            let square = document.createElement("div");
-            square.classList.add("square");
-            square.setAttribute("id", index + 1)
-            gameBoard.appendChild(square);
-            
-        }
     }
 });
